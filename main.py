2008cf08cc7d2beb0d7974829211cf7fc5e26309
Ducket191 <dangminhduc1912008@gmail.com>
2026-01-17 14:51:07 +0700

    update

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from decisionTree import classifier
 import os
 
 app = Flask(__name__)
@@ -16,26 +15,15 @@ def home():
 def receive():
     global latest_data
     latest_data = request.json
-    print("Received from Node:", latest_data)
     return jsonify({"status": "received"}), 200
 
 @app.route("/classify", methods=["POST"])
 def classify():
+    from decisionTree import classifier 
     data = request.json
-
-    try:
-        prediction = classifier.predict(data)
-        print("Prediction:", prediction)
-        return jsonify({"prediction": prediction[0]}), 200
-
-    except Exception as e:
-        print("Error during classification:", e)
-        return jsonify({"error": str(e)}), 500
+    prediction = classifier.predict(data)
+    return jsonify({"prediction": prediction[0]}), 200
 
 @app.route("/data", methods=["GET"])
 def data():
     return jsonify(latest_data)
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
